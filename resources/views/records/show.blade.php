81fb404af4a183de69e3536cdbf238f23743b1e2
suganuma-takuo <suganuma0606@gmail.com>
2023-06-11 12:38:04 +0900

    追加:出退勤履歴詳細表示機能

--- resources/views/records/show.blade.php
+++ resources/views/records/show.blade.php
@@ -1,15 +1,14 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_','-',app()->getLocale()) }}">
+<!DOCTYPE HTML>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <title>Record</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Posts</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
-        <h1>Records</h1>
-        <div class='records'>
-            <div class='record'>
+        <div class='record'>
             <table>
                 <tr>
                     <td>id</td>
@@ -20,9 +19,8 @@
                     <td>break_out</td>
                     <td>remarks</td>
                 </tr>
-            @foreach($records as $record)
                 <tr>
-                    <td><a href="/records/{{ $record->id }}">{{ $record->id }}</a> </td>
+                    <td>{{ $record->id }} </td>
                     <td>{{ $record->date }} </td>
                     <td>{{ $record->time_in }} </td>
                     <td>{{ $record->time_out }} </td>
@@ -30,10 +28,9 @@
                     <td>{{ $record->break_out }} </td>
                     <td>{{ $record->remarks }} </td>
                 </tr>
-            @endforeach
             </div>
-        </div>
-        <div class='paginate'>
-            {{ $records->links() }}
+        <div class="footer">
+            <a href="/">戻る</a>
         </div>
     </body>
+</html>
